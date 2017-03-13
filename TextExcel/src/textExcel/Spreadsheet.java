6f0641bc7fca57f63c996ad9036b73e77e44e423
rosalyncho <18chor67@millshighsch.org>
2017-03-12 20:03:29 -0700

    helpppp

--- TextExcel/src/textExcel/Spreadsheet.java
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -10,45 +10,50 @@ public class Spreadsheet implements Grid
 	Cell[][] excelSpreadsheet = new EmptyCell[20][12];	// 2D array of cells with 20 rows and 12 columns
 	
 	public Spreadsheet() {
+		for (int i = 0; i < excelSpreadsheet.length; i++){
+			for (int j = 0; j < excelSpreadsheet[i].length; j++){
+				excelSpreadsheet [i][j] = new EmptyCell();
+			}
+		}
 	}
 	
 	@Override
 	public String processCommand(String command)
 	{
 		// TODO Auto-generated method stub
-		String[] splitted = command.split(" ");
-		if (splitted.equals("clear")) {
-			if (splitted.length > 6) {
-				// clears the given cell
-				Location clear= new SpreadsheetLocation(splitted[1]);
-				Cell change=getCell(clear);
-				change=new EmptyCell();
-			} else {
-				// clears the entire spreadsheet
-				excelSpreadsheet=new Cell [20][12];
-				for(int i=0; i<20; i++){
-					for(int j=0; j<12;j++){
-						excelSpreadsheet[i][j]=new EmptyCell();
-					}
+		if(command.equals("")){
+			return command;
+		}
+		if (command.toLowerCase().equals("clear")) { // if the user input is "clear"
+			for(int i=0; i<20; i++){
+				for(int j=0; j<12;j++){
+					excelSpreadsheet[i][j]=new EmptyCell();
 				}
 			}
-		} else if (splitted[1].equals("=")) {
-			Location equals = new SpreadsheetLocation(splitted[0]);
-			Cell change = getCell(equals);
-			change = new TextCell(splitted[2]);
 			return getGridText();
-		} else if (splitted.length == 1) {
-			Location one = new SpreadsheetLocation(splitted[0]);
-			Cell n = getCell(one);
-			return n.fullCellText();
+		}// if the user types in "clear" plus a particular cell
+		else if (command.toLowerCase().equals("clear ")) {
+			String[] splitted = command.split(" ");
+			String location = splitted[1];
+			SpreadsheetLocation loc = new SpreadsheetLocation(location);
+			excelSpreadsheet[loc.getRow()][loc.getCol()] = new EmptyCell();
+		} //if command is for example "A1 = "hello""
+		else if (command.contains("=")) { 
+			String[] splitted = command.split(" ");
+			String location = splitted[0];
+			String str = splitted[1];
+			SpreadsheetLocation loc = new SpreadsheetLocation(location);
+			excelSpreadsheet[loc.getRow()][loc.getCol()] = new TextCell(str);
+			return getGridText();
 		}
-		return command;
+			SpreadsheetLocation loc = new SpreadsheetLocation(command);
+			return excelSpreadsheet[loc.getRow()][loc.getCol()].fullCellText();
+		
 	}
-
+	
 	@Override
 	public int getRows()
 	{
-		// TODO Auto-generated method stub
 		row = 20;
 		return row;
 	}
@@ -56,7 +61,6 @@ public class Spreadsheet implements Grid
 	@Override
 	public int getCols()
 	{
-		// TODO Auto-generated method stub
 		column = 12;
 		return column;
 	}
@@ -64,15 +68,29 @@ public class Spreadsheet implements Grid
 	@Override
 	public Cell getCell(Location loc)
 	{
-		// TODO Auto-generated method stub
-		return null;
+		return excelSpreadsheet[loc.getRow()][loc.getCol()];
 	}
 
 	@Override
 	public String getGridText()
 	{
-		// TODO Auto-generated method stub
-		
-		return null;
+		String grid = "   |";
+		for(char i = 'A'; i<='L'; i++){
+			grid += i + "         |";
+		}
+		System.out.println(grid);
+		String rowLabel = "";
+		for (int row = 0; row < getRows(); row++){
+			if (row < 9) {
+				rowLabel = (row+1) + "  |";
+			} else {
+				rowLabel = (row+1) + " |";
+			}
+			for(int j = 0; j<12; j++){
+				rowLabel += excelSpreadsheet[row][j].abbreviatedCellText() + "|";
+			}
+			System.out.println(rowLabel);
+		}
+		return "";
 	}
 }
